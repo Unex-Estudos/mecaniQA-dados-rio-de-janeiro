a0c1038017d8b6a4cba393f0bcfe630060ef8ec3
Cleyviton Ferreira <cleyviton@espressolabs.com.br>
2026-08-11 23:48:33 -0300

    feat: entrega dia 04/08 - Compreender a mecânica de adicionar dados no primeiro espaço vazio e discutir as consequências da remoção em alocações contíguas.

--- src/main/java/br/com/mecaniqa/Gerenciador.java
+++ src/main/java/br/com/mecaniqa/Gerenciador.java
@@ -6,4 +6,48 @@ public class Gerenciador {
 
     private int contadorPecas = 0;
     private int contadorServicos = 0;   
+
+    public void adicionarPeca(Peca peca) {
+        if (contadorPecas < pecas.length) {
+            pecas[contadorPecas++] = peca;
+        } else {
+            System.out.println("Limite de peças atingido.");
+        }
+    }
+
+    public void removerPeca(int codigo) {
+        for (int i = 0; i < contadorPecas; i++) {
+            if (pecas[i].codigo == codigo) {
+                for (int j = i; j < contadorPecas - 1; j++) {
+                    pecas[j] = pecas[j + 1];
+                }
+                pecas[--contadorPecas] = null;
+                System.out.println("Peça removida com sucesso.");
+                return;
+            }
+        }
+        System.out.println("Peça não encontrada.");
+    }
+
+    public void adicionarServico(Servico servico) {
+        if (contadorServicos < servicos.length) {
+            servicos[contadorServicos++] = servico;
+        } else {
+            System.out.println("Limite de serviços atingido.");
+        }
+    }
+
+    public void removerServico(int codigo) {
+        for (int i = 0; i < contadorServicos; i++) {
+            if (servicos[i].codigo == codigo) {
+                for (int j = i; j < contadorServicos - 1; j++) {
+                    servicos[j] = servicos[j + 1];
+                }
+                servicos[--contadorServicos] = null;
+                System.out.println("Serviço removido com sucesso.");
+                return;
+            }
+        }
+        System.out.println("Serviço não encontrado.");
+    }
 }
