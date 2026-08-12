8589841a86cdb8940fbf6e3442e612e988dfc7f6
Cleyviton Ferreira <cleyviton@espressolabs.com.br>
2026-08-12 00:34:15 -0300

    feat: entrega dia 11/08 - Criar o motor de busca sequencial, utilizar o acesso direto aos dados para modificar registros existentes.

--- src/main/java/br/com/mecaniqa/Gerenciador.java
+++ src/main/java/br/com/mecaniqa/Gerenciador.java
@@ -7,6 +7,7 @@ public class Gerenciador {
     private int contadorPecas = 0;
     private int contadorServicos = 0;   
 
+    /* Gerenciamento de Peças */
     public void adicionarPeca(Peca peca) {
         if (contadorPecas < pecas.length) {
             pecas[contadorPecas++] = peca;
@@ -15,20 +16,39 @@ public class Gerenciador {
         }
     }
 
-    public void removerPeca(int codigo) {
+    public int buscarPeca(int codigo) {
         for (int i = 0; i < contadorPecas; i++) {
             if (pecas[i].codigo == codigo) {
-                for (int j = i; j < contadorPecas - 1; j++) {
-                    pecas[j] = pecas[j + 1];
-                }
-                pecas[--contadorPecas] = null;
-                System.out.println("Peça removida com sucesso.");
-                return;
+                return i;
             }
         }
-        System.out.println("Peça não encontrada.");
+        return -1;
     }
 
+    public void atualizarPeca(int codigo, Peca novaPeca) {
+        int index = buscarPeca(codigo);
+        if (index != -1) {
+            pecas[index] = novaPeca;
+            System.out.println("Peça atualizada com sucesso.");
+        } else {
+            System.out.println("Peça não encontrada.");
+        }
+    }
+
+    public void removerPeca(int codigo) {
+        int index = buscarPeca(codigo);
+        if (index != -1) {
+            for (int i = index; i < contadorPecas - 1; i++) {
+                pecas[i] = pecas[i + 1];
+            }
+            pecas[--contadorPecas] = null;
+            System.out.println("Peça removida com sucesso.");
+        } else {
+            System.out.println("Peça não encontrada.");
+        }
+    }
+
+    /* Gerenciamento de Serviços */
     public void adicionarServico(Servico servico) {
         if (contadorServicos < servicos.length) {
             servicos[contadorServicos++] = servico;
@@ -37,17 +57,35 @@ public class Gerenciador {
         }
     }
 
-    public void removerServico(int codigo) {
+    public int buscarServico(int codigo) {
         for (int i = 0; i < contadorServicos; i++) {
             if (servicos[i].codigo == codigo) {
-                for (int j = i; j < contadorServicos - 1; j++) {
-                    servicos[j] = servicos[j + 1];
-                }
-                servicos[--contadorServicos] = null;
-                System.out.println("Serviço removido com sucesso.");
-                return;
+                return i;
             }
         }
-        System.out.println("Serviço não encontrado.");
+        return -1;
     }
+
+    public void atualizarServico(int codigo, Servico novoServico) {
+        int index = buscarServico(codigo);
+        if (index != -1) {
+            servicos[index] = novoServico;
+            System.out.println("Serviço atualizado com sucesso.");
+        } else {
+            System.out.println("Serviço não encontrado.");
+        }
+    }
+
+    public void removerServico(int codigo) {
+        int index = buscarServico(codigo);
+        if (index != -1) {
+            for (int i = index; i < contadorServicos - 1; i++) {
+                servicos[i] = servicos[i + 1];
+            }
+            servicos[--contadorServicos] = null;
+            System.out.println("Serviço removido com sucesso.");
+        } else {
+            System.out.println("Serviço não encontrado.");
+        }
+    }   
 }
